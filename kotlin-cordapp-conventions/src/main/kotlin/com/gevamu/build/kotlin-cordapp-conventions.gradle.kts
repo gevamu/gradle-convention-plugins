db3af477c1a5f9a25f0ea8a512eb0a82f537864f
Anton Sitnikov <anton.sitnikov@exactprosystems.com>
2023-08-22 18:12:13 +0400

    Align cordapp-conventions plugin with version from the Gevamu SDK

--- kotlin-cordapp-conventions/src/main/kotlin/com/gevamu/build/kotlin-cordapp-conventions.gradle.kts
+++ kotlin-cordapp-conventions/src/main/kotlin/com/gevamu/build/kotlin-cordapp-conventions.gradle.kts
@@ -37,11 +37,36 @@ dependencies {
 
     cordaRuntimeOnly("net.corda:corda:4.9.3")
 
-    // logging. Corda 4.9 provides log4j 2.17.1
+    // Logging. Corda 4.9.3 provides log4j 2.17.1
     cordaProvided("org.apache.logging.log4j:log4j-api:2.17.1")
-    cordaProvided("org.apache.logging.log4j:log4j-core:2.17.1")
 
     testImplementation("net.corda:corda-node-driver:4.9.3")
+}
+
+configurations.all {
+    resolutionStrategy {
+        dependencySubstitution {
+            all {
+                val requested = this.requested
+                if (requested is ModuleComponentSelector) {
+                    when (requested.group) {
+                        "com.fasterxml.jackson", "com.fasterxml.jackson.core" ->
+                            useTarget("${requested.group}:${requested.module}:2.13.3")
+                        "org.apache.logging.log4j" ->
+                            useTarget("${requested.group}:${requested.module}:2.17.1")
+                        "org.slf4j" ->
+                            useTarget("${requested.group}:${requested.module}:1.7.30")
+                    }
+                }
+            }
+            substitute(
+                module("com.fasterxml.jackson.module:jackson-module-kotlin")
+            ).using(
+                module("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
+            )
+            exclude("ch.qos.logback", "logback-classic")
+        }
+    }
 }
 
 tasks {
@@ -49,6 +74,8 @@ tasks {
         javaLauncher.set(javaToolchains.launcherFor {
             languageVersion.set(JavaLanguageVersion.of(8))
         })
+        minHeapSize = "512m"
+        maxHeapSize = "2048m"
     }
     jar {
         // This makes the JAR's SHA-256 hash repeatable.
